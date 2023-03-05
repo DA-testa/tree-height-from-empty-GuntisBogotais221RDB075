c3dd7fadc746f13fc7e1e1f5708fadf11eac8e99
GuntisBogotais221RDB075 <125827057+GuntisBogotais221RDB075@users.noreply.github.com>
2023-03-05 22:59:26 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,56 +2,53 @@
 
 import sys
 import threading
-import numpy
+import numpy as numerz
 
 
 def compute_height(n, parents):
-    # Write this function
-    root= -1
-    for i in range(n):
-        if parents[i]==-1:
-            root= i
-            break
-
-    tree=[[] for i in range(n)]
-    for i in range(n):
-        if parents[i]!=-1:
-            tree[parents[i]].append(i)
-    # Your code here
-    def max_height(node, depth):
-        if not tree[node]:
-            return depth
-        max_depth = depth
-        for child in tree[node]:
-            max_depth=max(max_depth, max_height(child, depth+1))
-        return max_depth
-    return max_height(root, 1)
+    high = numerz.zeros(int(n))
+    max_height = 0
+    for i in range(int(n)):
+        if high[i] >0:
+            height=0
+            j=1
+            while j !=-1:
+                if high[j]:
+                    height = height+height[j]
+                    break
+                else:
+                    height=height+1
+                    j = int(parents[j])
+                high[i]=height
+                if height > max_height:
+                    max_height=height
+    return max_height
 
 def main():
-    # implement input form keyboard and from files
-    check=input()
-    if check=="F":
-        filename = input()
-        with open(filename, 'r') as f:
-            n = int(f.readline())
-            parents = list(map(int, f.readline().split()))
+    input_method = input().strip()
+    if input_method =="I":
+        n=input().strip()
+        if n is not None:
+            parents = input().strip().split()
+            if parents:
+                height = compute_height(n, parents)
+                print(height)
     else:
-        n = int(input())
-        parents = list(map(int, input().split()))
+        file_location = input().strip()
+        try:
+            with open(f"./test/{file_location}") as f:
+                content = f.readlines()
+        except FileNotFoundError:
+            print("no file")
+            return
+        n=content[0].strip()
+        if n is not None:
+            parents = content[1].strip().split()
+            if parents:
+                height = compute_height(n, parents)
+                print(height)
 
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    print(compute_height(n, parents))
-    pass
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-# print(numpy.array([1,2,3]))
