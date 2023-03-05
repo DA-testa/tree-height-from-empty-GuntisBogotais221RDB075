2ab03c4aeabf47b480c4b54d5f8dfa8842ce5962
GuntisBogotais221RDB075 <125827057+GuntisBogotais221RDB075@users.noreply.github.com>
2023-03-05 14:56:57 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -29,7 +29,7 @@ def compute_height(n, parents):
 
 def main():
     # implement input form keyboard and from files
-    n = int(input())
+    n = input()
     n = int(input())
     parents = list(map(int, input().split()))
     # let user input file name to use, don't allow file names with letter a
